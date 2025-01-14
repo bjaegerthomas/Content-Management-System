1ea282819e59a8754eeaadfc9f85cc1812d6682b
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-14 11:41:43 -0500

    updated choices function for index.js

--- db/schema.sql
+++ db/schema.sql
@@ -13,20 +13,15 @@ CREATE TABLE role (
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER NOT NULL
-  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER DEFAULT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL REFERENCES role(id),
+  manager_id INTEGER DEFAULT NULL REFERENCES employee(id)
+  ON DELETE SET NULL
 );
--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import inquirer from "inquirer";
+
+function choices() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Welcome to your CMS Database. Please choose from the options below.",
+                name: "option",
+                choices: [
+                    "View all departments",
+                    "View all roles",
+                    "View all employees",
+                    "Add a department",
+                    "Add a Role",
+                    "Add an Employee",
+                    "Update an Employee role"
+                ],
+            },
+        ])
+        .then((response) => {
+            if (response.option === "View all departments") {
+                viewDepartments();
+            }
+            if (response.option === "View all roles") {
+                viewRoles();
+            }
+            if (response.option === "View all employees") {
+                viewEmployees();
+            }
+            if (response.option === "Add a department") {
+                addDepartment();
+            }
+            if (response.option === "Add a Role") {
+                addRole();
+            }
+            if (response.option === "Add an Employee") {
+                addEmployee();
+            }
+            if (response.option === "Update an Employee role") {
+                updateEmployee();
+            }
+        })
+        .catch((error) => {
+            console.error("An error occurred:", error);
+        });
+}
+
+choices();
