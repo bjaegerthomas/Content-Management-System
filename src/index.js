44b39ffb2c7745cfd9c4e499b9acca067ae8f247
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-14 15:48:54 -0500

    updated the addRole function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import inquirer from "inquirer";
 import express from 'express';
-import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
 
 await connectToDb();
@@ -25,7 +24,8 @@ function choices() {
                     "Add a department",
                     "Add a Role",
                     "Add an Employee",
-                    "Update an Employee role"
+                    "Update an Employee role",
+                    "Quit"
                 ],
             },
         ])
@@ -51,6 +51,10 @@ function choices() {
             if (response.option === "Update an Employee role") {
                 updateEmployee();
             }
+            if (response.option === "Quit") {
+                console.log("Goodbye!");
+                process.exit();
+            }
         })
         .catch((error) => {
             console.error("An error occurred:", error);
@@ -58,7 +62,167 @@ function choices() {
 }
 
 function viewDepartments() {
-    console.log("Viewing all departments");
+    pool.query('SELECT * FROM department', (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+        } else {
+            console.table(res.rows);
+            choices();
+        }
+    });
 }
+
+function viewRoles() {
+    pool.query('SELECT * FROM role', (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+        } else {
+            console.table(res.rows);
+            choices();
+        }
+    });
+}
+
+function viewEmployees() {
+    pool.query('SELECT * FROM employee', (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+        } else {
+            console.table(res.rows);
+            choices();
+        }
+    });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the name of the Department you would like to add:",
+                name: "department",
+            },
+        ])
+        .then((response) => {
+            pool.query('INSERT INTO department (name) VALUES ($1)', [response.department], (err, res) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                } else {
+                    console.log('Department added successfully.');
+                    choices();
+                }
+            });
+        });
+}
+
+function addRole() {
+    pool.query('SELECT id, name FROM department', (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+        } else {
+            const departmentChoices = res.rows.map(department => ({
+                name: department.name,
+                value: department.id
+            }));
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the name of the Role you would like to add:",
+                name: "role",
+            },
+            {
+                type: "input",
+                message: "Enter the Salary for this role:",
+                name: "salary",
+            },
+            {
+                type: "list",
+                message: "Enter the Department this role belongs to:",
+                name: "departmentId",
+                choices: departmentChoices
+            }
+        ])
+        .then((response) => {
+            pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [response.role, response.salary, response.departmentId], (err, res) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                } else {
+                    console.log('Role added successfully.');
+                    choices();
+                }
+                });
+            });
+        }
+    });
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the employee's First name:",
+                name: "firstName",
+            },
+            {
+                type: "input",
+                message: "Enter the employee's Last name:",
+                name: "lastName",
+            },
+            {
+                type: "input",
+                message: "Enter the employee's Role:",
+                name: "roleId",
+            },
+            {
+                type: "list",
+                message: "Enter the employee's Manager:",
+                name: "managerId",
+            },
+        ])
+        .then((response) => {
+            pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [response.firstName, response.lastName, response.roleId, response.managerId], (err, res) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                } else {
+                    console.log('Employee added successfully.');
+                    choices();
+                }
+            });
+        });
+}
+
+function updateEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter the employee's First name whom you would like to update:",
+                name: "employeeId",
+            },
+            {
+                type: "input",
+                message: "Enter the employee's Last name whom you would like to update:",
+                name: "employeeId",
+            },
+            {
+                type: "input",
+                message: "Enter the new Role ID for this employee:",
+                name: "roleId",
+            },
+        ])
+        .then((response) => {
+            pool.query('UPDATE employee SET role_id = $1 WHERE id = $2', [response.roleId, response.employeeId], (err, res) => {
+                if (err) {
+                    console.error('Error executing query', err.stack);
+                } else {
+                    console.log('Employee updated successfully.');
+                    choices();
+                }
+            });
+        });
+}
+
 
 choices();
