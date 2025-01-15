a63a21dc310ec2c43242f4f2d97b0ec911a5ea00
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-15 11:45:27 -0500

    debugged schema and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -12,8 +12,7 @@ CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL
-  REFERENCES department(id)
+  FOREIGN KEY (department_id) INTEGER NOT NULL REFERENCES department(id)
   ON DELETE SET NULL
 );
 
@@ -21,7 +20,7 @@ CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL REFERENCES role(id),
-  manager_id INTEGER DEFAULT NULL REFERENCES employee(id)
+  FOREIGN KEY (role_id) INTEGER NOT NULL REFERENCES role(id),
+  FOREIGN KEY (manager_id) INTEGER DEFAULT NULL REFERENCES employee(id)
   ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,25 @@
 INSERT INTO department (id, name)
-VALUES (001, ),
-       (002, ),
-       (003, ),
-       (004, );
+VALUES (001, Editorial),
+       (002, Design/Layout),
+       (003, Marketing),
+       (004, Sales);
 
 INSERT INTO role (id, title, salary, departemnt_id)
-VALUES (001, ),
-       (002, ),
-       (003, ),
-       (004, );
+VALUES (001, Editor in Chief, 120000, 001),
+       (002, Copy Editor, 50000, 001),
+       (003, Graphic Designer, 60000, 002),
+       (004, Marketing Manager, 70000, 003),
+       (005, Sales Manager, 80000, 004),
+       (006, Sales Associate, 40000, 004),
+       (007, Marketing Associate, 40000, 003),
+       (008, Creative Director, 100000, 002);
 
-INSERT INTO role (id, fiirst_name, last_name, role_id, manager_id)
-VALUES (001, ),
-       (002, ),
-       (003, ),
-       (004, );
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001, Ben, Thompson, 001, 002),
+       (002, Jane, Ellerby, 002, NULL),
+       (003, Jose, Garcia, 003, NULL),
+       (004, Mary, Faustin, 004, 007),
+       (005, Skip, Williams, 005, 006),
+       (006, Sarah, Brown, 006, NULL),
+       (007, Tarik, Davis, 007, NULL),
+       (008, Laura, Martinez, 008, 003);
